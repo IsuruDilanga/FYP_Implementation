e5e47a0d59cdad84733b746a168b222b37807a7b
Isuru Dissanayake <98075833+IsuruDilanga@users.noreply.github.com>
2024-02-13 00:07:34 +0530

    add home page

---
+++ aunite/src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from "react";
+import "./Home.css";
+import { Link } from "react-router-dom";
+
+const HomePage = () => {
+
+    return (
+        <div>
+            <h1>Home Page</h1>
+        </div>
+    );
+};
+
+export default HomePage;
